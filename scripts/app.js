2bd3a162b6fb58b000b8cae687455ea40d33b409
anja <119950303+letiperka@users.noreply.github.com>
2023-01-28 17:14:01 +0100

    updated app.js

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+const editPlayer1Btn = document.getElementById("edit-player-1-btn");
+const editPlayer2Btn = document.getElementById("edit-player-2-btn");
+
+editPlayer1Btn.addEventListener("click", openPlayerConfig);
+editPlayer2Btn.addEventListener("click", openPlayerConfig);
