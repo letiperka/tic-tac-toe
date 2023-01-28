3a00d1a649dc0c55d8c50046e09944899c6d4ab9
anja <119950303+letiperka@users.noreply.github.com>
2023-01-28 22:38:54 +0100

    added switching players functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,19 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
     {
         name: "",
-        symbol: "X"
+        symbol: "X",
     },
     {
         name: "",
-        symbol: "O"
+        symbol: "O",
     },
 ];
+
+const activePlayerName = document.getElementById("active-player-name");
+const winner = document.getElementById("winner-name");
 
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
@@ -19,6 +23,8 @@ const startGameBtn = document.getElementById("start-game-btn");
 
 const errorsOutput = document.getElementById("config-errors");
 const gameArea = document.getElementById("active-game");
+// const gameTiles = document.querySelectorAll("#game-board li");
+const gameBoard = document.getElementById("game-board")
 
 const form = document.querySelector("form");
 
@@ -34,3 +40,9 @@ backdrop.addEventListener("click", closePlayerConfig);
 form.addEventListener("submit", savePlayerConfig);
 
 startGameBtn.addEventListener("click", startNewGame);
+
+// for (const gameField of gameTiles) {
+//     gameField.addEventListener("click", selectGameField);
+// }
+
+gameBoard.addEventListener("click", selectGameField);
