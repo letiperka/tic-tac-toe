8f3dcc004977a603a337affa008d8e137e0c7a26
anja <119950303+letiperka@users.noreply.github.com>
2023-01-28 22:50:48 +0100

    updated game.js

--- scripts/game.js
+++ scripts/game.js
@@ -3,7 +3,7 @@ function startNewGame() {
         alert("Please set custom player names for both players!");
         return;
     }
-    
+
     activePlayerName.textContent = players[activePlayer].name;
     gameArea.style.display = "block";
 }
@@ -23,7 +23,20 @@ function selectGameField(event) {
         return;
     }
 
-    event.target.textContent = players[activePlayer].symbol;
-    event.target.classList.add("disabled");
+    const selectedField = event.target;
+    const selectedColumn = selectedField.dataset.col - 1;
+    const selectedRow = selectedField.dataset.row - 1;
+
+    if (gameData[selectedRow][selectedColumn] > 0) {
+        alert("Please select an empty field!");
+        return;
+    }
+
+    selectedField.textContent = players[activePlayer].symbol;
+    selectedField.classList.add("disabled");
+
+    gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
     switchPlayer();
 }
+
