59c471ce16bf9dc130da105c38108116081a89f3
anja <119950303+letiperka@users.noreply.github.com>
2023-01-28 17:27:07 +0100

    updated config.js and app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
+const cancelBtn = document.getElementById("cancel");
+// const confirmBtn = document.getElementById("confirm");
+
+const form = document.querySelector("form");
+
+const playerConfigOverlay = document.getElementById("config-overlay");
+const backdrop = document.getElementById("backdrop");
 
 editPlayer1Btn.addEventListener("click", openPlayerConfig);
 editPlayer2Btn.addEventListener("click", openPlayerConfig);
+
+cancelBtn.addEventListener("click", closePlayerConfig);
+backdrop.addEventListener("click", closePlayerConfig);
+
+form.addEventListener("submit", savePlayerConfig);
